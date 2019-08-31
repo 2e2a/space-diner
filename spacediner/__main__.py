a7b2c998e8897694036d5a9edf9f1c9472cb1d48
gvarab <marta@2e2a.de>
2019-08-31 19:42:49 +0200

    new yaml structure for social chatting

--- spacediner/__main__.py
+++ spacediner/__main__.py
@@ -5,8 +5,10 @@ from . import settings
 # TODO: Trash food on close-up
 # TODO: Do not allow to serve without taking the order first
 # TODO: Remove bonding from groups
-# TODO: Chatting with regulars: chat after serving; only available if satisfied
-# TODO: Chatting with regulars: chatting always increases bonding, good answers only speed it up
+# TODO: Chatting with regulars: chat after serving; only available if satisfied...
+# TODO: ...chatting always increases bonding, good answers only speed it up...
+# TODO: ...reward level specifies necessary number of good answers...
+# TODO: ...otherwise reward is triggered after all questions have been asked.
 # TODO: personalize review texts per group
 # TODO: not matching order -> max rating ok(2)
 # TODO: toggle verbose mode on/off --> more/less info in output
