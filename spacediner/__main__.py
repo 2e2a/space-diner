e63bb476fbd403a415830209cf1b6fe38e3a467a
gvarab <marta@2e2a.de>
2020-05-23 11:04:02 +0200

    add todos

--- spacediner/__main__.py
+++ spacediner/__main__.py
@@ -2,18 +2,19 @@ import argparse
 from . import cli
 from . import settings
 
+# TODO: dishes with 2 identical ingredients and one other not distinguishable from 1:2
 # TODO: skills should have an effect and influence reviews
 # TODO: make discovered preferences available in compendium
-# TODO: property matches in substring, e.g. fried = deep-fried
 # TODO: toggle tutorial mode on/off (after first day)
 # TODO: Fix output that plural is not needed
 # TODO: log all commands into file on exception
 # TODO: after increasing/decreasing a skill: show bar again (#----); add "Now your knife skills include: ..."
 # TODO: player can enter a name and will be addressed as "Chef [name]"
 # TODO: sometimes broken: NameFactory create (some names cause a problem?)
-# TODO: make skills available on specific days
+# TODO: make activities available on specific days
 # TODO: special weekend day (last day of the week) with two activities instead of work and one activity
 # TODO: think about menu: are all/some initial menu items fixed?
+
 
 parser = argparse.ArgumentParser(description='Space diner')
 parser.add_argument(
