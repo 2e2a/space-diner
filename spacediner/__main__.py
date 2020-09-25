d3cde33f34a172ac94ea3d7f3b9e2aa8243d405e
gvarab <marta@2e2a.de>
2020-09-25 13:23:35 +0200

    adjust level 2 (market, decoration)

--- spacediner/__main__.py
+++ spacediner/__main__.py
@@ -2,6 +2,8 @@ import argparse
 from . import cli
 from . import settings
 
+# TODO: bug: in reviews: "I liked None" / "I disliked None" (sth wrong with properties)
+# TODO: bug: showing recipe crashes (sth wrong with properties)
 # TODO: completer: remove underscore, use separate command parts instead
 # TODO: completer: support simple string command parts
 # TODO: completer: ignore capitalization and special characters before matching (see name_for_command)
