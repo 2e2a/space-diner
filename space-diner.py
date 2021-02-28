ab7d12270bd0dbf0749218e7a86a2b9a406113c2
2e2a <alexej@2e2a.de>
2021-02-28 18:02:11 +0100

    Use script instead of __main__
    works with pyinstaller

---
+++ space-diner.py
@@ -0,0 +1,23 @@
+import argparse
+from spacediner import cli, settings
+
+# TODO: rewards/trophies for achievements, e.g., served 10/50/100 dishes, received 10/50/100 positive reviews...
+# TODO: check if rewards by normal guests are possible
+# TODO: bug: sometimes prepared ingredients appear in the kitchen although they were already served
+# TODO: toggle tutorial mode on/off (after first day)
+# TODO: check non-ascii characters in name factories
+# TODO: sometimes broken: NameFactory create (some names cause a problem?)
+# TODO: make activities available on specific days
+# TODO: special weekend day (last day of the week) with two activities instead of work and one activity
+# TODO: think about menu: are all/some initial menu items fixed?
+
+parser = argparse.ArgumentParser(description='Space diner')
+parser.add_argument(
+    '-l', '--log',
+    action='store_true',
+    help='Log game input into separate file.',
+)
+args = vars(parser.parse_args())
+settings.read_args(args)
+cli.init()
+cli.run(args)
