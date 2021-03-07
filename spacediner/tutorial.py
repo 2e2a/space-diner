c54561d5d9fcfde7b328759c8bdd44e26d2f5ee3
gvarab <marta@2e2a.de>
2021-03-07 18:25:45 +0100

    work on tutorial and hints

--- spacediner/tutorial.py
+++ spacediner/tutorial.py
@@ -1,9 +1,33 @@
 from . import cli
 
 TUTORIAL = [
-    (cli.DinerMode, cli.DinerMode.CMD_TAKE_ORDER, 'Try to take an order.'),
-    (cli.DinerMode, cli.DinerMode.CMD_KITCHEN, 'Now go to the kitchen please.'),
-    (cli.KitchenMode, cli.KitchenMode.CMD_COOK, 'Cook!'),
+    (cli.DinerMode, cli.DinerMode.CMD_TAKE_ORDER,
+     'Welcome! This short tutorial is going to guide through the first couple of steps. First, try to take an order. '
+     'The command is "take order from [NAME]". Use auto-complete: type "take" and press TAB, then type the first '
+     'letters of a customer\'s name and press TAB again.'
+     ),
+    (cli.DinerMode, cli.DinerMode.CMD_KITCHEN,
+     'After taking orders, you can go to the kitchen to prepare the food: "go to kitchen".'
+     ),
+    (cli.KitchenMode, cli.KitchenMode.CMD_COOK,
+     'Every dish consists of three ingredients. Once you have prepared them, they will be automatically plated as '
+     'a completed dish. If you like, you can look at available recipes (type: "recipes"). To start cooking, '
+     'type "[VERB] [INGREDIENT]". The available verbs (e.g., "grill", "plate"...) and ingredients are listed above.'
+     ),
+    (cli.KitchenMode, cli.KitchenMode.CMD_COOK,
+     'Now you can add a second ingredient.'
+     ),
+    (cli.KitchenMode, cli.KitchenMode.CMD_COOK,
+     'Add a third ingredient.'
+     ),
+    (cli.KitchenMode, cli.KitchenMode.CMD_DINER,
+     'Once you have completed a dish, return to your guests: "go to diner".'
+     ),
+    (cli.DinerMode, cli.DinerMode.CMD_SERVE,
+     'Now it\'s time to serve your dish: "serve [DISH] to [NAME]". After serving all the customers, you can '
+     'call it a day ("close up"). New guests will come tomorrow. (This is the end of the tutorial. You can '
+     'type "help" at any point during the game for hints and a list of available commands.)'
+     ),
 ]
 
 
