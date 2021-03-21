14d8521c62626b74a645f835a1013342e346c12f
gvarab <marta@2e2a.de>
2021-03-21 20:30:26 +0100

    manage todos

--- spacediner/__main__.py
+++ spacediner/__main__.py
@@ -1,9 +1,7 @@
-# TODO: consider showing available or suggested commands when starting a level
-# TODO: define win-message per level
+# TODO: autocompletion for moss/moss-milk is broken
 # TODO: rewards/trophies for achievements, e.g., served 10/50/100 dishes, received 10/50/100 positive reviews...
 # TODO: check if rewards by normal guests are possible
 # TODO: bug: sometimes prepared ingredients appear in the kitchen although they were already served
-# TODO: toggle tutorial mode on/off (after first day)
 # TODO: check non-ascii characters in name factories
 # TODO: sometimes broken: NameFactory create (some names cause a problem?)
 # TODO: make activities available on specific days
